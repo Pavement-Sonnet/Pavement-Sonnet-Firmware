2598b623e908d2e4df129f635ee4438a22fca788
Justin Lin <61717681+justinlin099@users.noreply.github.com>
2025-11-17 17:18:49 +0100

    Update: Hardware Test
    Complete all Hardware Sensor Test Codes.
    Sensor List:
    IMU
    GPS
    Temp
    Sound

--- LMU-Test/src/main.cpp
+++ LMU-Test/src/main.cpp
@@ -2,14 +2,36 @@
 #include <Adafruit_MPU6050.h>
 #include <Adafruit_Sensor.h>
 #include <Wire.h>
+#include <WiFi.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+#define RXD2 16
+#define TXD2 17
+#define GPS_BAUD 9600
+const int tempPin = 32;
+const int soundPin = 35;
+
+HardwareSerial gpsSerial(2);
 
 Adafruit_MPU6050 mpu;
 
-// put function declarations here:
+OneWire oneWire(tempPin);
+DallasTemperature tempSensor(&oneWire);
 
+// WiFi Configuration
+const char ssid[]="Justin_[object Object]";
+const char pwd[]="66966696";
 
 void setup() {
   Serial.begin(115200);
+  WiFi.mode(WIFI_STA); //設置WiFi模式
+  WiFi.begin(ssid,pwd);
+  gpsSerial.begin(GPS_BAUD, SERIAL_8N1, RXD2, TXD2);
+
+  // Activate the DS18B20 sensor
+  tempSensor.begin();
+  
   while (!Serial) {
     delay(10);
   }
@@ -55,15 +77,27 @@ void setup() {
 void loop() {
   sensors_event_t a, g, temp;
   mpu.getEvent(&a, &g, &temp);
+  tempSensor.requestTemperatures();
+  int tempValue = tempSensor.getTempCByIndex(0);
+  int soundValue = analogRead(soundPin);
 
   
-  Serial.printf("%.3f %.3f %.3f %.3f %.3f %.3f\n", 
+  Serial.printf("%.3f %.3f %.3f %.3f %.3f %.3f %d %d\n", 
                 a.acceleration.x, 
                 a.acceleration.y, 
                 a.acceleration.z,
                 g.gyro.x,
                 g.gyro.y,
-                g.gyro.z);
+                g.gyro.z,
+                tempValue,
+                soundValue
+              );
+
+  while (gpsSerial.available() > 0){
+    // get the byte data from the GPS
+    char gpsData = gpsSerial.read();
+    Serial.print(gpsData);
+  }
   
   delay(100);
 }

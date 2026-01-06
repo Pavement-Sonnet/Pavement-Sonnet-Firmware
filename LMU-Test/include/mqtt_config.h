0cd4b683c56aa4763670f6971f0363c730bbe3f5
Halit Yagar <halityagar45@gmail.com>
2026-01-07 00:22:24 +0100

    Update .gitignore and MQTT configuration for improved clarity and consistency

--- LMU-Test/include/mqtt_config.h
+++ LMU-Test/include/mqtt_config.h
@@ -7,7 +7,7 @@
 #define WIFI_PASS ""
 
 // Broker can be your PC running Mosquitto (use PC IP) or a remote broker
-#define MQTT_BROKER_IP "1.1.1.1"
+#define MQTT_BROKER_IP "test.mosquitto.org"
 #define MQTT_BROKER_PORT 1883
 
 // Client ID and optional credentials (leave empty strings for no auth)
@@ -16,7 +16,9 @@
 #define MQTT_PASS ""
 
 // Topics
-#define MQTT_TOPIC_SUB "test/esp32/in"
-#define MQTT_TOPIC_PUB "test/esp32/out"
+#define MQTT_TOPIC_SUB "esp32/server/in"
+#define MQTT_TOPIC_GPS "esp32/device2/sensors/gps"
+#define MQTT_TOPIC_ACCEL "esp32/device2/sensors/accel"
+#define MQTT_TOPIC_SENSORS "esp32/device2/sensors/data"
 
 #endif // MQTT_CONFIG_H
